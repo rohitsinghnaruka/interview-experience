0f3e174f05925ef1ce96caa48b3df366f98db01a
Rohit Singh <rohitnaruka6852@gmail.com>
2025-07-04 00:05:47 +0530

    Update axios.js with deployed url of backend
    removed localhost url with render deployed backend url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ Use your backend URL
+  baseURL: "https://interview-experience-j3p6.onrender.com/api", // ✅ Use your backend URL
   withCredentials: true, // allow cookies (optional for now)
 });
 
