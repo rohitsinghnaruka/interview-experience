c9a98d9781a7b7bbaf8b0ac9781dd56a47a4fd9b
om-tayade-1920 <omtayade86@gmail.com>
2025-07-07 13:18:34 +0530

    uploaded files

--- server/index.js
+++ server/index.js
@@ -11,21 +11,13 @@ import userRouter from './routes/userRoute.js';
 import errorMiddleware from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
 import blogRouter from './routes/blogRoute.js';
-// import path from 'path';
+
 import commentRouter from './routes/commentRoute.js';
 
-
-
-
-// const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(cors({
-//   origin: 'http://localhost:5173',   // ✅ must match frontend origin
-//   credentials: true                 // ✅ allows cookies and auth headers
-// }));
 app.use(cors({
   origin: 'https://interview-experience-vnit.vercel.app',
   credentials: true,
@@ -35,26 +27,7 @@ app.use('/api/user', userRouter);
 app.use('/api/blog', blogRouter);
 app.use('/api/comment', commentRouter);
 
-// this commenting for backedtesting without react frontend
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// import path from 'path';
-
-
-// app.use(express.static(path.join(__dirname, '/client/dist')));
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-// });
-
 app.use(errorMiddleware);
-
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
